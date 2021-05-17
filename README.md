488082de58098d17f59a2251073a6e2cd2f8e20a
Scott Starkey <yekrats@gmail.com>
2021-05-17 18:20:24 -0400

    Optimizations of old code. (loops -> reduces)

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Stegosaurus
 
-I'm a magician in addition to being a coder. I used Clojure to help develop the algorithm behind a magic trick to create my
+I'm a magician in addition to being a software developer. I used Clojure to help develop the algorithm behind a magic trick to create my
 own version of the trick. The original trick (not invented by me) is called "Stegosaurus" and marketed by Penguin Magic.
 I currently sell my enhancements made by my Clojure developments on The Game Crafter.
 
@@ -12,23 +12,24 @@ The code is used for my Animal Themed deck at https://www.thegamecrafter.com/gam
 
 The original algorithm of the magic trick is a binary code of letters. 
 In the base effect, cards either have letters for the left side of the keyboard or the right side.
-The list of 32 5-letter words is numbered 0 to 31. The first letter position is worth 16 points, the second letter is worth 8 points, and so on, so
-that each word has a unique position in the list in binary numbers. So, the word TIGER is equal to 19, because it is T (16 points), 
-I (does not score 8 points), G (does not score 4 points), E (scores 2 points), R (scores 1 point). The letters scoring their binary position are 
-roughly the left side of the QWERTY keyboard. A word like ONION, all on the right side of the keyboard, scores a 0. 
+The list of 32 5-letter words is numbered 0 to 31. The first letter position is worth 16 points, the second letter is worth 
+8 points, and so on, so that each word has a unique position in the list in binary numbers. So, the word TIGER is equal to 19, 
+because it is T (16 points), I (does not score 8 points), G (does not score 4 points), E (scores 2 points), R (scores 1 point). 
+The letters scoring their binary position are roughly the left side of the QWERTY keyboard. A word like ONION, all on the right 
+side of the keyboard, scores a 0. 
 
-The `score-word` function accepts a word and an optional regex. (If no regex is supplied, it will use the default regex as in the original effect.)
-It will provide the binary score of the word given.
+The `score-word` function accepts a word and an optional regex. (If no regex is supplied, it will use the default regex as in the 
+original effect.) It will provide the binary score of the word given.
 
 The `all-english-words-sorted` function (no parameters) will show each of the scores for all 5-letter words in the dictionary. 
 Call like `(all-english-words-sorted)`.
 
 The def `alpha-combo2` is what I used to pare the 2 million combinations of regexes down to 6 possibilities to fill every slot of my 
-animals.txt word list.  ***Warning!*** This takes hours to realize the full lazy sequence on my old MacBook Pro. If I made this again, I would probably
-lean on multi-threading to reduce the time. However, something like `(first alfpha-combo2)` is safe and much faster. Once I determined the 6 usable
-regexes, I plugged them into a simple loop to give me the proper animal list to use. (See lines 179-190.)
+animals.txt word list.  ***Warning!*** This takes hours to realize the full lazy sequence on my old MacBook Pro. If I made this again, 
+I would probably lean on multi-threading to reduce the time. However, something like `(first alfpha-combo2)` is safe and much faster. 
+Once I determined the 6 usable regexes, I plugged them into a simple loop to give me the proper animal list to use. (See lines 179-190.)
 
 ## License
 
-Copyright © 2020.
+Copyright © 2021.
 Do not reproduce without permission.
--- src/further_testing/core.clj
+++ src/further_testing/core.clj
@@ -3,37 +3,7 @@
   (:require [clojure.math.combinatorics :as combo])
   )
 
-(defn check-letter?
-  ([letter]
-   (check-letter? letter "[qwertasdfgzxcv]"))
-
-  ([letter re]
-   (if (nil? re)
-     (re-find #"[qwertasdfgzxcv]" letter)
-     (re-find (re-pattern re) letter)
-     ))
-  )
-
-(defn score-word
-  ([word re]
-   (loop [x 0 acc 0]
-     (if (= x (count word))
-       acc
-       (recur
-         (+ x 1)
-         (+ acc
-            (if (check-letter? (str (nth word x)) re)
-              (reduce * (repeat (- 4 x) 2))
-              0))))))
-  ([word]
-   (score-word word "[qwertasdfzxcvbQWERTASDFZXCVB]")))
-
-(defn check-letter
-  ([letter]
-   (check-letter? letter "[qwertasdfgzxcv]"))
-
-  ([letter re]
-   (check-letter? letter re)))
+(def stegregex "[qwertasdfzxcvQWERTASDFZXCV]")
 
 (def words
   (->> (slurp "./resources/google-10000-english-usa.txt") ; (slurp "/usr/share/dict/words")
@@ -50,19 +20,6 @@
        set
        (filter #(= 5 (count %)))))
 
-(defn is31? "Checks to see if a particular wordlist has a perfect score of 31"
-  [re wordlist]
-  (loop [i 0 f false]
-    (if (or (= i 32) f)
-      (and (not f) (= i 32))
-      (recur
-        (inc i)
-        (= (count (filter #(= i (score-word % (re-pattern re))) wordlist)) 0)
-        )
-      )
-    )
-  )
-
 (def steg
   (->> (slurp "./resources/steg.txt")
        str/split-lines
@@ -71,29 +28,45 @@
        )
   )
 
-(defn regex-score "Takes in a regex and a wordlist. Provides a score. This is how many hits for the wordlist & regex."
+(defn score-word
+  "Scores the word according to the Stegosaurus algorithm, or with a supplied regex."
+  ([word re]
+   (reduce (fn [acc cur]
+             (if (re-find (re-pattern re) (str (nth word cur))) ; Check if the letter is in the regex.
+               (+ acc (int (Math/pow 2 (- 4 cur)))); Add in the power of two, if it matches.
+               acc)) ; Otherwise, keep the accumulator the same.
+           0 ; Reduction starting with 0.
+           (range 5))) ; Over the course of all 5 letters.
+  ([word]
+    (score-word word "[qwertasdfzxcvbQWERTASDFZXCVB]")))
+
+(defn is32? "Checks to see if a particular wordlist has a perfect score of 32."
   [re wordlist]
+  (reduce (fn [acc cur]
+            (and acc (seq (filter #(= cur (score-word % (re-pattern re))) wordlist))))
+          true
+          (range 32)))
+
+(defn regex-score
+    "Takes in a regex and a wordlist.
+    Provides a score. This is how many hits for the wordlist & regex."
                                         ; (regex-score "[qwertasdfzxcvb]" animals)
                                         ; => 26
+  [re wordlist]
+  (reduce (fn [acc cur]
+            (if (seq (filter #(= cur (score-word % (re-pattern re))) wordlist))
+              ; "If there are some in the wordlist matching the score number... then inc acc."
+              (inc acc)
+              acc))
+          0
+          (range 32)))
 
-  (loop [x 0 acc 0]
-    (if (= x 32)
-      acc
-      (recur
-       (+ x 1)
-       (if (> (count (filter #(= x (score-word % (re-pattern re))) wordlist)) 0)
-         (+ acc 1)
-         acc)))))
-
-(def alpha-combo2
+(def alpha-combo
   (as-> "abcdefghijklmnopqrstuvwxyz" $
         (str/split $ #"")                                 ; whole list split up individually
         (combo/combinations $ 13)                         ; first = ("a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m")
         (map #(str "[" (apply str %) "]") $)              ; ("[abcdefghijklm]" "[abcdefghijkln]") etc.
-        ; (map (fn [re] {:re re :sc (regex-score re animals)})  %) $
-        (filter #(is31? % animals) $)
-        )                                                 ; ({:re "[abcdefghijklm]", :sc 19} ...)
-  )
+        (filter #(is32? % animals) $)))
 
 (defn all-english-words-sorted []
   (loop [x 0]
@@ -231,9 +204,111 @@
   (defn dupe [& [farg & rargs :as args]]
     (when (seq args)
       (cons farg (cons farg (apply dupe rargs)))))
+(defn score-word
+  ([word re]
+   (loop [x 0 acc 0]
+     (if (= x (count word))
+       acc
+       (recur
+         (inc x)
+         (+ acc
+            (if (check-letter? (str (nth word x)) re)
+              (int (Math/pow 2 (- 4 x)))
+              0))))))
+  ([word]
+   (score-word word "[qwertasdfzxcvbQWERTASDFZXCVB]")))
+
+(defn score-word2
+  "Second attempt at score-word, to create a more elegant algorithm."
+  ([word re]
+   (reduce +
+           (for [pos (range (count word))
+                :let [score (int (Math/pow 2 (- 4 pos)))]
+                :when (re-find (re-pattern re) (str (nth word pos)))]
+             score)))
+
+             ; This for loop will create a series of powers of 2, but only keep them
+             ; when the letter is matched within the regex. Essentially it does the
+             ; same thing as in the original score-word defn. However, the elegance
+             ; comes at a small cost.
+             ;
+             ; (time (dotimes [_ 100000] (score-word "party")))
+             ; "Elapsed time: 533.07543 msec
+             ;
+             ;(time (dotimes [_ 100000] (score-word2 "party")))
+             ; "Elapsed time: 597.83245 msecs"
 
 
+  ([word]
+    (score-word2 word "[qwertasdfzxcvbQWERTASDFZXCVB]")))
+
+(defn score-word3
+  "Third attempt at score-word, to create a more elegant algorithm."
+  ([word re]
+   (reduce (fn [acc cur]
+             (if (re-find (re-pattern re) (str (nth word cur))) ; Check if the letter is in the regex.
+               (+ acc (int (Math/pow 2 (- 4 cur)))); Add in the power of two, if it matches.
+               acc)) ; Otherwise, keep the accumulator the same.
+           0 ; Reduction starting with 0.
+           (range 5)) ; Over the course of all 5 letters.
+  ([word]
+    (score-word3 word "[qwertasdfzxcvbQWERTASDFZXCVB]")))
+
+  ; This version is both elegant and efficient!
+  ; further-testing.core> (time (dotimes [_ 100000] (score-word3 "party")))
+  ; "Elapsed time: 506.824872 msecs"
+
+
+(defn check-letter
+  ([letter]
+   (check-letter? letter "[qwertasdfgzxcv]"))
+
+  ([letter re]
+   (check-letter? letter re)))
+
+(defn regex-score ; old version. New version is more than 2x faster.
+                    ;  (time (dotimes [_ 1000] (regex-score "[adefghjlpsuvx]" animals)))
+                    ;      "Elapsed time: 2877.598247 msecs"
+                    ;  (time (dotimes [_ 1000] (regex-score2 "[adefghjlpsuvx]" animals)))
+                    ;      "Elapsed time: 1112.193068 msecs"
+
+    "Takes in a regex and a wordlist.
+    Provides a score. This is how many hits for the wordlist & regex."
+  [re wordlist]
+                                        ; (regex-score "[qwertasdfzxcvb]" animals)
+                                        ; => 26
+
+  (loop [x 0 acc 0]
+    (if (= x 32)
+      acc
+      (recur
+       (+ x 1)
+       (if (> (count (filter #(= x (score-word % (re-pattern re))) wordlist)) 0)
+         (+ acc 1)
+         acc)))))
+
+(defn check-letter?
+  ([letter]
+   (check-letter? letter "[qwertasdfgzxcv]"))
+
+  ([letter re]
+   (if (nil? re)
+     (re-find #"[qwertasdfgzxcv]" letter)
+     (re-find (re-pattern re) letter)
+     ))
+  )
+
+(defn is31? "Checks to see if a particular wordlist has a perfect score of 31"
+  [re wordlist]
+  ; Deprecated. Reimplemented by is32?
+  (loop [i 0 f false]
+    (if (or (= i 32) f)
+      (and (not f) (= i 32))
+      (recur
+        (inc i)
+        (= (count (filter #(= i (score-word % (re-pattern re))) wordlist)) 0)))))
 
   (__ 0 [:a :b :c]) {:a 0 :b 0 :c 0})
 ;
 ;end comment
+)
